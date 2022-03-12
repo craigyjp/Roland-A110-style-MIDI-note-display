f1f21feaa985aef1ff46185ee16f78d088f1b8bc
craigyjp <51026305+craigyjp@users.noreply.github.com>
2022-03-12 22:19:35 +0200

    Add files via upload

--- A110_spi_display.ino
+++ A110_spi_display.ino
@@ -3,6 +3,142 @@
 #include <MIDI.h>
 #include <USBHost_t36.h>
 #include <SPI.h>
+#include <TM1637Display.h>
+
+// Module connection pins (Digital Pins)
+#define CLK 34
+#define DIO 33
+
+// The amount of time (in milliseconds) between tests
+#define TEST_DELAY   2000
+
+const uint8_t CHAN01[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_B | SEG_C                                    // 1
+};
+
+const uint8_t CHAN02[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_D | SEG_E | SEG_G                           // 2
+};
+
+const uint8_t CHAN03[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_D | SEG_C | SEG_G                                // 3
+};
+
+const uint8_t CHAN04[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_B | SEG_C | SEG_F | SEG_G                                    // 4
+};
+
+const uint8_t CHAN05[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G                                 // 4
+};
+
+const uint8_t CHAN06[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_C | SEG_D | SEG_E | SEG_F | SEG_G                                // 4
+};
+
+const uint8_t CHAN07[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_C                              // 4
+};
+
+const uint8_t CHAN08[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E  | SEG_F | SEG_G                               // 4
+};
+
+const uint8_t CHAN09[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_C | SEG_F | SEG_G                               // 4
+};
+
+const uint8_t CHAN10[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                    // 1
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F   // 0
+};
+
+const uint8_t CHAN11[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                   // 1
+  SEG_B | SEG_C                                    // 1
+};
+
+const uint8_t CHAN12[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                    // 1
+  SEG_A | SEG_B | SEG_D | SEG_E | SEG_G                           // 2
+};
+
+const uint8_t CHAN13[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                   // 1
+  SEG_A | SEG_B | SEG_D | SEG_C | SEG_G                                // 3
+};
+
+const uint8_t CHAN14[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                    // 1
+  SEG_B | SEG_C | SEG_F | SEG_G
+};
+
+const uint8_t CHAN15[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                    // 1
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G            // 5
+};
+
+const uint8_t CHAN16[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                    // C
+  SEG_C | SEG_E | SEG_F | SEG_G,                   // h
+  SEG_B | SEG_C,                                    // 1
+  SEG_C | SEG_D | SEG_E | SEG_F | SEG_G            // 6
+};
+
+const uint8_t OMNI[] = {
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // 0
+  SEG_A | SEG_B | SEG_C | SEG_E | SEG_F,           // M
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_C                                            // i
+};
+
+const uint8_t PGM[] = {
+  SEG_A | SEG_B | SEG_E | SEG_F | SEG_G,            // P
+  SEG_E | SEG_G,                                    // r
+  SEG_A | SEG_C | SEG_D | SEG_E | SEG_F,            // G
+  SEG_A | SEG_B | SEG_C | SEG_E | SEG_F             // M
+};
+
+TM1637Display display(CLK, DIO);
 
 #define DISPLAY_NORMAL 0
 #define DISPLAY_OVERVIEW 1
@@ -14,8 +150,13 @@ typedef struct {
   uint16_t param; //the parameter that is selected
 } param_type;
 
-int masterChan = 1;
+int mynotes[96];
 
+int ProgramMode = 0;
+int ProgramChangeReceived = 0;
+int newChan = 0;
+int masterChan = 1;
+int potvalue = 0;
 int firstoctavenote = 0;
 int secondoctavenote = 0;
 int thirdoctavenote = 0;
@@ -90,6 +231,8 @@ const uint8_t SCRMAP[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 #define OLED_DC7     30
 #define OLED_DC8     31
 
+#define SUSTAINLED     35
+
 Adafruit_SSD1306 display1(SCREEN_WIDTH, SCREEN_HEIGHT,
                           &SPI, OLED_DC1, OLED_RESET, OLED1_CS);
 
@@ -128,23 +271,23 @@ void setup() {
   //USB HOST MIDI Class Compliant
   delay(200); //Wait to turn on USB Host
   myusb.begin();
-  //  midi1.setHandleControlChange(myControlChange);
-  //  midi1.setHandleProgramChange(myProgramChange);
+  midi1.setHandleControlChange(myControlChange);
+  midi1.setHandleProgramChange(myProgramChange);
   midi1.setHandleNoteOff(myNoteOff);
   midi1.setHandleNoteOn(myNoteOn);
   Serial.println("USB HOST MIDI Class Compliant Listening");
 
   //USB Client MIDI
-  //  usbMIDI.setHandleControlChange(myControlChange);
-  //  usbMIDI.setHandleProgramChange(myProgramChange);
+  usbMIDI.setHandleControlChange(myControlChange);
+  usbMIDI.setHandleProgramChange(myProgramChange);
   usbMIDI.setHandleNoteOff(myNoteOff);
   usbMIDI.setHandleNoteOn(myNoteOn);
   Serial.println("USB Client MIDI Listening");
 
   //MIDI 5 Pin DIN
   MIDI.begin();
-  //  MIDI.setHandleControlChange(myControlChange);
-  //  MIDI.setHandleProgramChange(myProgramChange);
+  MIDI.setHandleControlChange(myControlChange);
+  MIDI.setHandleProgramChange(myProgramChange);
   MIDI.setHandleNoteOn(myNoteOn);
   MIDI.setHandleNoteOff(myNoteOff);
   Serial.println("MIDI In DIN Listening");
@@ -155,7 +298,271 @@ void setup() {
   //Next, we splash the OLED screens
   setupScreens();
   splashScreens();
+  display.setBrightness(0x0f);
+  pinMode(SUSTAINLED, OUTPUT);
+  digitalWrite(SUSTAINLED, LOW);
+  for (int i = 0; i < 96; i++)
+  {
+    mynotes[i] = 0;
+  }
 }
+
+void myProgramChange(byte channel, byte program)
+{
+  if (ProgramMode == 1 )
+  {
+    display.showNumberDec((program + 1), false); // Expect: ___1
+    ProgramChangeReceived = 1;
+  }
+
+}
+
+void myControlChange(byte channel, byte control, byte value)
+{
+  if (control == 64 )
+  {
+    if (value >= 64)
+    {
+      digitalWrite(SUSTAINLED, HIGH);
+    }
+    else
+    {
+      digitalWrite(SUSTAINLED, LOW);
+    }
+  }
+}
+
+void clear_notes()
+{
+  for (int i = 0; i < 96; i++)
+  {
+    if (mynotes[i] == 1)
+    {
+      myNoteOff(0, i, 0);
+      mynotes[i] = 0;
+    }
+  }
+}
+
+void read_pot()
+{
+  potvalue = analogRead(A0);
+
+  if (potvalue >= 0 && potvalue < 57)
+  {
+    newChan = 1;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 1;
+    display.setSegments(CHAN01);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 58 && potvalue < 114 )
+  {
+    newChan = 2;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 2;
+    display.setSegments(CHAN02);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 115 && potvalue < 171)
+  {
+    newChan = 3;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 3;
+    display.setSegments(CHAN03);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 172 && potvalue < 227)
+  {
+    newChan = 4;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 4;
+    display.setSegments(CHAN04);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 228 && potvalue < 283)
+  {
+    newChan = 5;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 5;
+    display.setSegments(CHAN05);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 284 && potvalue < 339)
+  {
+    newChan = 6;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 6;
+    display.setSegments(CHAN06);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 340 && potvalue < 395)
+  {
+    newChan = 7;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 7;
+    display.setSegments(CHAN07);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 396 && potvalue < 451)
+  {
+    newChan = 8;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 8;
+    display.setSegments(CHAN08);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 452 && potvalue < 507)
+  {
+    newChan = 9;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 9;
+    display.setSegments(CHAN09);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 508 && potvalue < 563)
+  {
+    newChan = 10;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 10;
+    display.setSegments(CHAN10);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 564 && potvalue < 619)
+  {
+    newChan = 11;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 11;
+    display.setSegments(CHAN11);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 620 && potvalue < 675)
+  {
+    newChan = 12;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 12;
+    display.setSegments(CHAN12);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 676 && potvalue < 731)
+  {
+    newChan = 13;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 13;
+    display.setSegments(CHAN13);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 732 && potvalue < 787)
+  {
+    newChan = 14;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 14;
+    display.setSegments(CHAN14);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 788 && potvalue < 843)
+  {
+    newChan = 15;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 15;
+    display.setSegments(CHAN15);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 844 && potvalue < 899)
+  {
+    newChan = 16;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 16;
+    display.setSegments(CHAN16);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 900 && potvalue < 955)
+  {
+    newChan = 0;
+    if (newChan != masterChan)
+    {
+      clear_notes();
+    }
+    masterChan = 0;
+    display.setSegments(OMNI);
+    ProgramMode = 0;
+    ProgramChangeReceived = 0;
+  }
+  else if (potvalue >= 956 && potvalue < 1024)
+  {
+    masterChan = 0;
+    ProgramMode = 1;
+    if (ProgramChangeReceived != 1)
+    {
+      display.setSegments(PGM);
+    }
+  }
+}
+
 
 void myNoteOn(byte channel, byte note, byte velocity)
 {
@@ -175,6 +582,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 9:
     case 10:
     case 11:
+      mynotes[note] = 1;
       firstoctavenote = note;
       display_note(firstoctavenote, 0);
       break;
@@ -191,6 +599,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 21:
     case 22:
     case 23:
+      mynotes[note] = 1;
       secondoctavenote = (note - 12);
       display_note(secondoctavenote, 1);
       break;
@@ -207,6 +616,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 33:
     case 34:
     case 35:
+      mynotes[note] = 1;
       thirdoctavenote = (note - 24);
       display_note(thirdoctavenote, 2);
       break;
@@ -223,6 +633,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 45:
     case 46:
     case 47:
+      mynotes[note] = 1;
       fourthoctavenote = (note - 36);
       display_note(fourthoctavenote, 3);
       break;
@@ -239,6 +650,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 57:
     case 58:
     case 59:
+      mynotes[note] = 1;
       fifthoctavenote = (note - 48);
       display_note(fifthoctavenote, 4);
       break;
@@ -255,6 +667,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 69:
     case 70:
     case 71:
+      mynotes[note] = 1;
       sixthoctavenote = (note - 60);
       display_note(sixthoctavenote, 5);
       break;
@@ -271,6 +684,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 81:
     case 82:
     case 83:
+      mynotes[note] = 1;
       seventhoctavenote = (note - 72);
       display_note(seventhoctavenote, 6);
       break;
@@ -287,6 +701,7 @@ void myNoteOn(byte channel, byte note, byte velocity)
     case 93:
     case 94:
     case 95:
+      mynotes[note] = 1;
       eigthoctavenote = (note - 84);
       display_note(eigthoctavenote, 7);
       break;
@@ -312,6 +727,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 9:
     case 10:
     case 11:
+      mynotes[noteoff] = 0;
       firstoctavenoteoff = noteoff;
       remove_note(firstoctavenoteoff, 0);
       break;
@@ -328,6 +744,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 21:
     case 22:
     case 23:
+      mynotes[noteoff] = 0;
       secondoctavenoteoff = (noteoff - 12);
       remove_note(secondoctavenoteoff, 1);
       break;
@@ -344,6 +761,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 33:
     case 34:
     case 35:
+      mynotes[noteoff] = 0;
       thirdoctavenoteoff = (noteoff - 24);
       remove_note(thirdoctavenoteoff, 2);
       break;
@@ -360,6 +778,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 45:
     case 46:
     case 47:
+      mynotes[noteoff] = 0;
       fourthoctavenoteoff = (noteoff - 36);
       remove_note(fourthoctavenoteoff, 3);
       break;
@@ -376,6 +795,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 57:
     case 58:
     case 59:
+      mynotes[noteoff] = 0;
       fifthoctavenoteoff = (noteoff - 48);
       remove_note(fifthoctavenoteoff, 4);
       break;
@@ -392,6 +812,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 69:
     case 70:
     case 71:
+      mynotes[noteoff] = 0;
       sixthoctavenoteoff = (noteoff - 60);
       remove_note(sixthoctavenoteoff, 5);
       break;
@@ -408,6 +829,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 81:
     case 82:
     case 83:
+      mynotes[noteoff] = 0;
       seventhoctavenoteoff = (noteoff - 72);
       remove_note(seventhoctavenoteoff, 6);
       break;
@@ -424,6 +846,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
     case 93:
     case 94:
     case 95:
+      mynotes[noteoff] = 0;
       eigthoctavenoteoff = (noteoff - 84);
       remove_note(eigthoctavenoteoff, 7);
       break;
@@ -432,6 +855,7 @@ void myNoteOff(byte channel, byte noteoff, byte velocity)
 }
 
 void loop() {
+  read_pot();
   // put your main code here, to run repeatedly:
   myusb.Task();
   midi1.read(masterChan);   //USB HOST MIDI Class Compliant
